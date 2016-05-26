d99f8ee26bd8f3d49a05f0ac9b6584bcd965b72f
Arda Özkal <arda.ozkal@studentpartner.com>
2016-05-26 15:51:29 +0300

    Release

---
+++ Tabula/Tabula/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Tabula
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer dt = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            dt.Interval = new TimeSpan(0, 0, 1);
+            dt.Tick += Dt_Tick;
+            dt.Start();
+        }
+
+        private void Dt_Tick(object sender, EventArgs e)
+        {
+            dt.Stop();
+            var lines = File.ReadAllLines("tabu.txt");
+            var count = 0;
+            foreach (string line in lines)
+            {
+                count++;
+                var splitted = line.Split(',');
+                textBlock.Text = splitted[0];
+                textBlock_Copy.Text = string.Format("{1}{0}{2}{0}{3}{0}{4}{0}", Environment.NewLine + Environment.NewLine, splitted[1], splitted[2], splitted[3], splitted[4]);
+                Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.ApplicationIdle, new Action(() => { })).Wait();
+                util.SaveCanvas(this, this.canvas1, 96 * 3, "out" + count + ".png");
+            }
+
+            Application.Current.Shutdown();
+        }
+    }
+    public static class util
+    {
+        public static void SaveWindow(Window window, int dpi, string filename)
+        {
+
+            var rtb = new RenderTargetBitmap(
+                (int)window.Width * (dpi / 96), //width
+                (int)window.Width * (dpi / 96), //height
+                dpi, //dpi x
+                dpi, //dpi y
+                PixelFormats.Pbgra32 // pixelformat
+                );
+            rtb.Render(window);
+
+            SaveRTBAsPNG(rtb, filename);
+
+        }
+
+        public static void SaveCanvas(Window window, Canvas canvas, int dpi, string filename)
+        {
+            Size size = new Size(window.Width, window.Height);
+            canvas.Measure(size);
+            //canvas.Arrange(new Rect(size));
+
+            var rtb = new RenderTargetBitmap(
+                (int)window.Width * (dpi / 96), //width
+                (int)window.Height * (dpi / 96), //height
+                dpi, //dpi x
+                dpi, //dpi y
+                PixelFormats.Pbgra32 // pixelformat
+                );
+            rtb.Render(canvas);
+
+            SaveRTBAsPNG(rtb, filename);
+        }
+
+        private static void SaveRTBAsPNG(RenderTargetBitmap bmp, string filename)
+        {
+            var enc = new System.Windows.Media.Imaging.PngBitmapEncoder();
+            enc.Frames.Add(System.Windows.Media.Imaging.BitmapFrame.Create(bmp));
+
+            using (var stm = System.IO.File.Create(filename))
+            {
+                enc.Save(stm);
+            }
+        }
+    }
+}
